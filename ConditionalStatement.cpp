b027f676d74798aa1c4cf289fb9d0ab9e65a4215
RafiiArifNugroho <rafialamcitra@gmail.com>
2026-03-10 16:17:08 +0700

    Menambahkan fungsi hitungBMI

--- ConditionalStatement.cpp
+++ ConditionalStatement.cpp
@@ -10,3 +10,9 @@ void inputData(float &berat, float &tinggi)
     cout << "Masukkan tinggi badan (meter): ";
     cin >> tinggi;
 }
+
+// Fungsi menghitung BMI
+float hitungBMI(float berat, float tinggi)
+{
+    return berat / (tinggi * tinggi);
+}
